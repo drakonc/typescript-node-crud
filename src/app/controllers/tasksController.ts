14812eff681da6c20bfcabe94fbc272d803a15b8
Jose Castro <drakonc@gmail.com>
2019-10-29 15:55:34 -0500

    FInalizado

--- src/app/controllers/tasksController.ts
+++ src/app/controllers/tasksController.ts
@@ -5,15 +5,19 @@ import Task from "../models/tasksModel";
 
 class TasksController {
 
-    public index(req: Request, res: Response) {
+    public createForm(req: Request, res: Response) {
         res.status(200).render('tasks/create');
     }
 
     public async create(req: Request, res: Response) {
         const { titulo, descripcion } = req.body;
-        const newTask = new Task({ titulo, descripcion })
-        await newTask.save();
-        res.status(200).redirect('/tasks/list')
+        try {
+            const newTask = new Task({ titulo, descripcion })
+            await newTask.save();
+            res.status(200).redirect('/tasks/list')
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     public async list(req: Request, res: Response) {
@@ -21,6 +25,36 @@ class TasksController {
         res.status(200).render('tasks/list', { tasks });
     }
 
+    public async delete(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            await Task.findByIdAndDelete(id);
+            res.status(200).redirect('/tasks/list')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async editForm(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            const task = await Task.findById(id)
+            res.status(200).render('tasks/edit', { task });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async edit(req: Request, res: Response) {
+        const { id } = req.params;
+        const { titulo, descripcion } = req.body;
+        try {
+            await Task.findByIdAndUpdate(id, { titulo, descripcion })
+            res.status(200).redirect('/tasks/list');
+        } catch (error) {
+
+        }
+    }
 }
 
 const tasksController = new TasksController();
--- src/app/routes/tasksRoute.ts
+++ src/app/routes/tasksRoute.ts
@@ -11,9 +11,12 @@ class TasksRouter {
     }
 
     private config(): void {
-        this.router.get('/create', tasksController.index);
-        this.router.post('/create', tasksController.create)
-        this.router.get('/list', tasksController.list)
+        this.router.get('/create', tasksController.createForm);
+        this.router.post('/create', tasksController.create);
+        this.router.get('/list', tasksController.list);
+        this.router.get('/delete/:id', tasksController.delete);
+        this.router.get('/edit/:id', tasksController.editForm);
+        this.router.post('/edit/:id', tasksController.edit);
     }
 
 }
